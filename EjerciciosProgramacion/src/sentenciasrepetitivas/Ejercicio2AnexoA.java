dfb7908c5474f416f118027390d4500288a342a6
Olmo <olmot.s@gmail.com>
2018-10-16 12:19:12 +0200

    Octavo Commit: Tabla de datos f(x)=1/x

--- EjerciciosProgramacion/src/sentenciasrepetitivas/Ejercicio2AnexoA.java
+++ EjerciciosProgramacion/src/sentenciasrepetitivas/Ejercicio2AnexoA.java
@@ -8,14 +8,18 @@ public class Ejercicio2AnexoA {
 			/*Hacer una tabla de la función f(x)=1/x, con 11 valores, comenzando en
 			un número negativo A y terminando en –A, los valores de x deben estar
 			igualmente espaciados. Note que la serie pasará por x=0.*/
+	float x=-6;
+	float y;
 	
+	for(int i=1; i<=11; i++){
+		
+		x++;
+		y=1/x;
+		System.out.println("Para x =" + x + " y es igual a " + y);
+		
 	
-	float x;
-	float y=(1/x);
-	System.out.println("Valor de x ");
-	x = teclado.nextFloat();
+	}
 	
-	for( ; x<=10; )
 	{
 	}
 	}
---
+++ EjerciciosProgramacion/src/sentenciasrepetitivas/Fraccion.java
@@ -0,0 +1,5 @@
+package sentenciasrepetitivas;
+
+public class Fraccion {
+
+}
