e2d4d1569b41d4f5fc3935bebb84b9731f43693d
Olmo <olmot.s@gmail.com>
2018-10-30 10:08:14 +0100

    Décimo Octavo Commit: Volcán y mosaico

--- EjerciciosProgramacion/src/sentenciasrepetitivas/DibujosConAsteriscos.java
+++ EjerciciosProgramacion/src/sentenciasrepetitivas/DibujosConAsteriscos.java
@@ -1,9 +1,44 @@
 package sentenciasrepetitivas;
 
-public class CalcularElCuboDeUnNúmero {
+public class DibujosConAsteriscos {
 
+	static void volcan (int h) {
+		int asteriscos = 2;
+		int espacios;
+		int total = (int)Math.pow(2,h);
+		
+		for(int i = 0; i<h; i++) {
+			espacios = total - asteriscos/2;
+			for(int j=0;j<espacios; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 0; j < asteriscos; j++ ) {
+				System.out.print("*");
+			}
+			asteriscos+=asteriscos;
+			System.out.println();
+			
+		}
+		
+	}
+	
+	static void mosaico (int f, int c) {
+		for(int i = 0; i < f; i++) {
+			for(int j = 0; j < c; j++) {
+				if((j % 2 != 0 && i % 2 == 0) || ( j % 2 == 0 && i % 2 != 0 )) {
+					System.out.print('6');
+				}else {
+					System.out.print('+');
+				}
+				
+			}
+			System.out.println();
+		}
+	}
+	
 	public static void main(String[] args) {
 		
+		mosaico (8,8);
 
 	}
 
