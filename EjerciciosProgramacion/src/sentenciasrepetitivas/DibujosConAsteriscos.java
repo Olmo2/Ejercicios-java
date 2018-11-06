8aafb441e375982f79f1a5049f51f6a0c489daae
Olmo <olmot.s@gmail.com>
2018-11-06 08:52:57 +0100

    Décimo noveno Commit: Tablero Deberes

--- EjerciciosProgramacion/src/sentenciasrepetitivas/DibujosConAsteriscos.java
+++ EjerciciosProgramacion/src/sentenciasrepetitivas/DibujosConAsteriscos.java
@@ -2,43 +2,62 @@ package sentenciasrepetitivas;
 
 public class DibujosConAsteriscos {
 
-	static void volcan (int h) {
+	static void volcan(int h) {
 		int asteriscos = 2;
 		int espacios;
-		int total = (int)Math.pow(2,h);
-		
-		for(int i = 0; i<h; i++) {
-			espacios = total - asteriscos/2;
-			for(int j=0;j<espacios; j++) {
+		int total = (int) Math.pow(2, h);
+
+		for (int i = 0; i < h; i++) {
+			espacios = total - asteriscos / 2;
+			for (int j = 0; j < espacios; j++) {
 				System.out.print(" ");
 			}
-			for(int j = 0; j < asteriscos; j++ ) {
+			for (int j = 0; j < asteriscos; j++) {
 				System.out.print("*");
 			}
-			asteriscos+=asteriscos;
+			asteriscos += asteriscos;
 			System.out.println();
-			
+
 		}
-		
+
 	}
-	
-	static void mosaico (int f, int c) {
-		for(int i = 0; i < f; i++) {
-			for(int j = 0; j < c; j++) {
-				if((j % 2 != 0 && i % 2 == 0) || ( j % 2 == 0 && i % 2 != 0 )) {
-					System.out.print('6');
-				}else {
-					System.out.print('+');
+
+	static void mosaico(int f, int c) {
+		for (int i = 0; i < f; i++) {
+			for (int j = 0; j < c; j++) {
+				if ((j % 2 != 0 && i % 2 == 0) || (j % 2 == 0 && i % 2 != 0)) {
+					System.out.print("+++");
+				} else {
+					System.out.print("   ");
 				}
-				
+
 			}
+
 			System.out.println();
 		}
+
 	}
-	
+
+	static void tablero(int f, int c) {
+		for (int i = 0; i < f; i++) {
+			for (int x = 0; x < 3; x++) {
+				for (int j = 0; j < c; j++) {
+					if ((j % 2 != 0 && i % 2 == 0) || (j % 2 == 0 && i % 2 != 0)) {
+						System.out.print("+");
+					} else {
+						System.out.print(" ");
+					}
+
+				}
+
+				System.out.println();
+			}
+		}
+	}
+
 	public static void main(String[] args) {
-		
-		mosaico (8,8);
+
+		mosaico(8, 8);
 
 	}
 
